547e264040455e34a55aa54be751211774a1f694
KevinTenorio <kevin.tenorio98@gmail.com>
2024-04-11 18:46:25 -0300

    fix: message in the root request

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 import uvicorn
 
 from controllers import routes
 
-app = FastAPI(title="Poker Bot")
+app = FastAPI(title="Poker Bot do Kevin")
 
 for router in routes:
     app.include_router(router)
 
 @app.get("/", tags=["Root"], summary='Root endpoint')
-def read_root():
-    return {"message": "Go to http://127.0.0.1:8000/docs to see the API documentation."}
+def read_root(request: Request):
+    return {"message": f'Go to {request.base_url}docs to see the API documentation.'}
 
 if __name__ == "__main__":
     uvicorn.run(app='main:app', reload=True, port=8000)
